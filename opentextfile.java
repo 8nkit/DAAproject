a92de930b745a9ac0ec9b1a2b516c4f6e4600180
Ankit Sharma <ankitvashisht@hotmail.com>
2019-04-21 22:17:49 +0530

    2nd

---
+++ opentextfile.java
@@ -0,0 +1,17 @@
+package io;
+import java.nio.file.*;;
+public class ReadTextAsString
+{
+  public static String readFileAsString(String fileName)throws Exception
+  {
+    String data = "";
+    data = new String(Files.readAllBytes(Paths.get(fileName)));
+    return data;
+  }
+
+  public static void main(String[] args) throws Exception
+  {
+    String data = readFileAsString("C:\\Users\\pankaj\\Desktop\\test.java");
+    System.out.println(data);
+  }
+} 
