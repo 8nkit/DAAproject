48a16a04a5b2cc84e38e243f4326030918cbd22d
Ankit Sharma <ankitvashisht@hotmail.com>
2019-05-14 09:35:42 +0530

    Signed-off-by: Ankit Sharma <ankitvashisht@hotmail.com>

---
+++ superfinalcrux/Edge.java
@@ -0,0 +1,9 @@
+package finalcrux;
+public class Edge{
+	  public final Vertex target;
+		public final double weight;
+		public Edge(Vertex target, double weight){
+			this.target = target;
+			this.weight = weight;
+		}
+	}
